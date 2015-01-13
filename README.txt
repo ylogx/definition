4e5b065eb6cab704f8dee71d7002e0c6cf357d10
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-01-14 00:29:17 +0530

    Release on PyPi

---
+++ README.txt
@@ -0,0 +1,18 @@
+Definition
+=========
+
+Fetch definition from 5 or more online dictionaries
+Available easily from PyPi
+
+#### Installation
+
+```sh
+pip install definition
+```
+
+#### Usage
+To fetch definition of <word>
+
+```sh
+definition <word>
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+add_keywords = dict(
+    entry_points = {
+        'console_scripts': ['enter = definition.definition:main'],
+    },
+)
+
+setup(
+        name='definition',
+        description='Definition fetcher from 5 online dictionaries',
+        version='0.0.1',
+        packages=['definition'],
+        license='GPLv3+',
+        author='Shubham Chaudhary',
+        author_email='me@shubhamchaudhary.in',
+        url='https://github.com/shubhamchaudhary/definition',
+        long_description=open('README.txt').read(),
+        **add_keywords
+)
+
